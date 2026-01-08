d7f2ea1a66ee719961087de6eaa9395d772962ac
kanzaki-git <ashpem6mp@gmail.com>
2026-01-09 08:07:16 +0900

    マス数を指定できる盤面

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,10 @@
 class BoardsController < ApplicationController
   def index
-    @cells = (1..30).to_a
+    size = params[:size].to_i
+    size = 30 if size <= 0
+    size = 200 if size > 200 # 上限（暴走防止）
+
+    @size = size
+    @cells = (1..@size).to_a
   end
 end
