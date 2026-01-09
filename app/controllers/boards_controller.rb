b39701acc54ec2e6a33dfc0379aa571eec2f08c9
kanzaki-git <ashpem6mp@gmail.com>
2026-01-10 08:01:37 +0900

    ダイスの追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,10 +1,11 @@
 class BoardsController < ApplicationController
   def index
     size = params[:size].to_i
-    size = 15 if size <= 15
+    size = 15 if size < 15
     size = 200 if size > 200 # 上限（暴走防止）
 
     @size = size
     @cells = (1..@size).to_a
+    @dice = rand(1..6) if params[:roll].present?
   end
 end
